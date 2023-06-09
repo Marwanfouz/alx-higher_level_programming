54094d76df8be51d7e8ceb3db9245eb85b242f8a
Marwan Fouz <marwanfouz76@gmail.com>
2023-06-10 00:18:25 +0300

    tasks

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def add(a, b):
+        """My addition function
+
+        Args:
+            a: first integer
+            b: second integer
+
+        Returns:
+            The return value. a + b
+        """
+        return (a + b)
