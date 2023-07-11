74aa826a3993c388efee1642e09cdb6d342ffa4b
Marwan Fouz <marwanfouz76@gmail.com>
2023-07-11 18:35:39 +0300

    first task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@
 
 def read_file(filename=""):
     """Function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, encoding='utf-8') as f :
-        for i in f :
-            print(i,end="")
+    with open(filename, encoding='utf-8') as f:
+        for i in f:
+            print(i, end="")
