5a2653020eecbbb88852ccb209175d9ca17fb076
Marwan Fouz <marwanfouz76@gmail.com>
2023-07-11 18:30:15 +0300

    initialize first task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    this module contains a function that reads a text file
+    (UTF8) and prints it
+"""
+
+
+def read_file(filename=""):
+    """Function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding='utf-8') as f :
+        for i in f :
+            print(i,end="")
