1b90ee8e0e37ef91e4da4c128002e8b9f2100144
Marwan Fouz <marwanfouz76@gmail.com>
2023-07-14 14:52:15 +0300

    10 task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -20,4 +20,5 @@ class Student:
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student."""
         if type(attrs) == list:
-            return self.__dict__
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
