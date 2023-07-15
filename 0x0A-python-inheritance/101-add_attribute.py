3ad0ee35d5c10f5933c4869d59dca97bd26a4f7c
Marwan Fouz <marwanfouz76@gmail.com>
2023-07-15 22:37:53 +0300

    1 advanced task

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
+"""this module contains a function that adds a new attribute to an object if"""
+"""it's possible"""
 
 
 def add_attribute(obj, name, value):
+    """adds a new attribute to an object if it's possible"""
     if hasattr(obj, "__dict__"):
         setattr(obj, name, value)
     else:
