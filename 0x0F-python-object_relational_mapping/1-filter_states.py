818872a1d427ff520dd5519673c5221a736151ac
Marwan Fouz <marwanfouz76@gmail.com>
2023-09-19 17:22:54 +0300

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,9 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT * \
     FROM states \
-    WHERE name LIKE\
-    'N%';")
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
     states = cur.fetchall()
 
     for state in states:
