3d9063b911a4493804539f54554012b38aff7568
Marwan Fouz <marwanfouz76@gmail.com>
2023-10-26 11:30:53 +0300

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const number = Number(process.argv[2]);
+if (isNaN(number)) {
+    console.log('Missing size');
+} else {
+    let mySquare;
+    for (let i = 0; i < number; i++){
+        mySquare = '';
+        for (let j = 0; j < number; j++){
+            mySquare += 'X';
+        }
+        console.log(mySquare);
+    }
+}
---
+++ 0x12-javascript-warm_up/test.js
@@ -0,0 +1,8 @@
+const number = Number(process.argv[2]);
+if (isNaN(number)){
+    console.log('Missing number of occurrences')
+}else {
+    for (let i = 0; i < number; i++){
+        console.log('x');
+    }
+}
