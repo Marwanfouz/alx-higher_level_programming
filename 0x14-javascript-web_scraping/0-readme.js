985c172e097ba42a581ba982a6126ed81e601e0b
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-16 19:02:26 +0200

    fix style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
-    return;
   }
   console.log(data);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3],'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.error(err);
-    return;
   }
 });
