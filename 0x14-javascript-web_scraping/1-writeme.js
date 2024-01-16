7d2b796a579cc8df758c8014432d57232621af43
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-16 18:59:52 +0200

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3],'utf-8', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
 });
