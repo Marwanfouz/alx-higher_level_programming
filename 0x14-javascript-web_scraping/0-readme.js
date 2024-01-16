4e13af37071ccd3a36022d01e8ca891906c4ebf3
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-16 18:35:47 +0200

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data)
+});
---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,2 @@
+const headerElement = document.querySelector('header');
+headerElement.style.color = '#FF0000';
