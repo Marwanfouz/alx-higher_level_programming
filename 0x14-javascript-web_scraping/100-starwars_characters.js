2a8b93c13cb6cccc102512c06baa55d9f32f5f80
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-17 15:55:16 +0200

    7. Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (err, res, body) => {
+  if (err || res.statusCode !== 200) {
+    console.error(err);
+    return;
+  }
+  const data = JSON.parse(body).characters;
+  for (const characters of data) {
+    request.get(characters, (err, res, body) => {
+      if (err || res.statusCode !== 200) {
+        console.error(err);
+        return;
+      }
+      const movie = JSON.parse(body);
+      console.log(movie.name)
+    });
+  }
+});
