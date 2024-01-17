d362ffcc057e8e61e01baaa4c764804533357348
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-17 14:30:11 +0200

    6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,6 +1,6 @@
 #!/user/bin/node
 const request = require('request');
-const url = process.argv[2]
+const url = process.argv[2];
 request(url, (err, res, body) => {
   if (err || res.statusCode !== 200) {
     console.error(err);
