4a66cc9a46ccc770bf98b3e8cb4b496488050d2d
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-17 13:49:28 +0200

    5. Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const reqeust = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filename = process.argv[3];
+reqeust(url, (err, res, body) => {
+  if (err || res.statusCode !== 200) {
+    console.error(err);
+    return;
+  }
+  fs.writeFile(filename, body ,'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  })
+});
